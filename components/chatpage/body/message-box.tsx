316e5e159533644082d10659fbf4935df5e85271
hcy <1306574172@qq.com>
2025-02-28 20:54:01 +0800

    各别样式调整

--- components/chatpage/body/message-box.tsx
+++ components/chatpage/body/message-box.tsx
@@ -8,24 +8,24 @@ import { LoaderCircle } from 'lucide-react';
 
 interface MessageBoxProps {
     message: Doc<"messages">;
-    userImageUrl?: string; 
+    userImageUrl?: string;
     model?: string;
 }
 
 function MessageBox ({
-    message,
-    userImageUrl,
-    model
-}: MessageBoxProps) {
-    
+                         message,
+                         userImageUrl,
+                         model
+                     }: MessageBoxProps) {
+
     // 如果 role 是 system，直接返回 null，不渲染
     if (message.role === "system") {
         return null;
-    } 
+    }
 
-    const nameString = message.role === "user" ? "You" : "TalkGPT";
-    const imageUrl = message.role === "user" ? userImageUrl : (model===GPTModel.KIMI?'/kimi_log.svg':'/coze_log.svg');
-    const hasAttachments = message.attachmentMetaInfoList && message.attachmentMetaInfoList.length > 0; 
+    const nameString = message.role === "user" ? "You" : (model === GPTModel.KIMI ? "TalkKimi" : "TalkCoze");
+    const imageUrl = message.role === "user" ? userImageUrl : (model === GPTModel.KIMI ? '/kimi_log.svg' : '/coze_log.svg');
+    const hasAttachments = message.attachmentMetaInfoList && message.attachmentMetaInfoList.length > 0;
     return (
         <div className={`flex pb-8 ${message.role === "user" ? "flex-row-reverse" : "flex-row"}`}>
             <Avatar className="w-7 h-7 text-white fill-white">
@@ -36,11 +36,13 @@ function MessageBox ({
             </Avatar>
             <div className="max-w-[calc(80%)] px-2">
                 <h3 className={`font-bold ${message.role === "user" ? "text-end" : "text-start"}`}>{nameString}</h3>
-                {message.role!=="user"&&message.content==="" && (<div>
-                    <LoaderCircle className="w-4 h-4 animate-spin" />
-                </div>)}
+                {message.role !== "user" && message.content === "" && (
+                    <div>
+                        <LoaderCircle className="w-4 h-4 animate-spin" />
+                    </div>
+                )}
                 <div className={`flex flex-grow flex-col gap-3 ${message.role === "user" ? "items-end" : "items-start"}`}>
-                    <Markdown content={message.content} role={message.role}/> 
+                    <Markdown content={message.content} role={message.role}/>
                     {hasAttachments && (
                         <AttachmentDisplay attachmentMetaInfoList={message.attachmentMetaInfoList}/>
                     )}
@@ -53,9 +55,8 @@ function MessageBox ({
 // 自定义比较函数
 const arePropsEqual = (prevProps: MessageBoxProps, nextProps: MessageBoxProps ) => {
     // 用户的肯定不再次渲染了
-    if(nextProps.message.role==='user') return true;
+    if(nextProps.message.role === 'user') return true;
     return (
-        
         prevProps.message._id === nextProps.message._id &&
         prevProps.message.content === nextProps.message.content &&
         prevProps.model === nextProps.model
@@ -65,3 +66,6 @@ const arePropsEqual = (prevProps: MessageBoxProps, nextProps: MessageBoxProps ) 
 const MemoizedMessageBox = React.memo(MessageBox, arePropsEqual );
 
 export { MemoizedMessageBox as MessageBox };
+
+
+
