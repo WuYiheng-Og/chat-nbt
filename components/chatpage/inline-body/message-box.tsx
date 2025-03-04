0f3e1a2676b8cd8e1fe7ba7b299672df444bcb41
戴森球 <142326902+daisenqiu399@users.noreply.github.com>
2025-03-04 20:09:04 +0800

    Update 内联框样式

--- components/chatpage/inline-body/message-box.tsx
+++ components/chatpage/inline-body/message-box.tsx
@@ -66,7 +66,7 @@ function MessageBox ({
 
     return (
         <>
-            <div className='w-full'>
+            <div className='w-11/12'>
                 <Avatar className="w-5 h-5 text-white fill-white">
                     <AvatarImage src={imageUrl} className="text-white fill-white" />
                     <AvatarFallback className="text-neutral-900 font-semibold">
@@ -90,7 +90,7 @@ function MessageBox ({
                                 <LoaderCircle className="w-4 h-4 animate-spin" />
                             </div>
                         )}
-                        <div className='w-full'>
+                        <div className='w-11/12'>
                             <Markdown content={message.content} role={message.role}/>
                             {hasAttachments && (
                                 <AttachmentDisplay attachmentMetaInfoList={message.attachmentMetaInfoList}/>
